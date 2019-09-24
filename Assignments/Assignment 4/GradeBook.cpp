ca85c6273f155e6e19807fc73857242fd795a660
CristianCortez <49254113+CristianCortez@users.noreply.github.com>
2019-09-24 12:53:40 -0700

    Add files via upload

---
+++ Assignments/Assignment 4/GradeBook.cpp
@@ -0,0 +1,19 @@
+#include "GradeBook.h"
+
+Semester::Semester() {
+	cout << "obj built.";
+}
+
+void Semester::setNumP(int num) {
+	numPrograms = num;
+}
+void Semester::setNumT(int num) {
+	numTest = num;
+}
+void Semester::setNumF(int num) {
+	numFinal = num;
+}
+void Semester::setWeigths(int num, int idx) {
+	weights[idx] = num;
+}
+

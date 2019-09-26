511cd7b315945188c1374166b27d8cf5b546f499
paulochu02 <55256907+paulochu02@users.noreply.github.com>
2019-09-25 18:10:23 -0700

    Add files via upload

--- Assignments/Assignment 4/GradeBookUtility.cpp
+++ Assignments/Assignment 4/GradeBookUtility.cpp
@@ -17,93 +17,112 @@ void runDescription() {
 
 void runBookMenu() {
 	char option;
-
 	bool sFlag = false;
-	bool notTrue;
-
 	int numsOf = 0;
-	int total = 0;
-
-	string name;
-
+	Student *headStudent;
 	Semester smt;
-	Student stu;
-	Grades grd;
-	
+
+	int total = 0;
 	do {
-		notTrue = false;
 		cout << " Please Enter an option (S, A, P, T, F, C, G, O, Q) :";
 		cin >> option;
 		toupper(option);
 		switch (option) {
 		case 'S':
 			do {
-				cout << "How many programming assignments are there? (0-6)";
+				cout << "How many programming assignments are there? (0-6)" << endl;
 				cin >> numsOf;
-				if (numsOf < 0 || numsOf > 6) {
+				if (numsOf < 0 || numsOf > 6)
 					cout << "\nPlease enter correct amt of assignments." << endl;
-				}
-				else {
-					smt.setNumP(numsOf);
-					notTrue = true;
-				}
-			} while (notTrue);
-			notTrue = false;
+				else
+					smt.setSizeP(numsOf);
+			} while (numsOf < 0 || numsOf > 6);
+			cout << "How many tests are there? (0-4) " << endl;
+			cin >> numsOf;
+			smt.setSizeT(numsOf);
+
+			cout << "How many final exams are there? (0-1) " << endl;
+			cin >> numsOf;
+			smt.setSizeF(numsOf);
 			do {
-				cout << "How many tests are there? (0-4) ";
-				cin >> numsOf;
-				if (numsOf < 0 || numsOf > 4) {
-					cout << "\nPlease enter correct amt of test." << endl;
-				}
-				else {
-					smt.setNumT(numsOf);
-					notTrue = true;
-				}
-			} while (notTrue);
-			notTrue = false;
-			do {
-				cout << "How many final exams are there? (0-1) ";
-				cin >> numsOf;
-				if (numsOf < 0 || numsOf > 1)
-					cout << "\nPlease enter correct amt of exams." << endl;
-				else {
-					smt.setNumF(numsOf);
-					notTrue = true;
-				}
-			} while (notTrue);
-			do {
-				cout << "Please enter weights (1-9) for: "
-					<< "\n\t Tests: ";
+				cout << "Please enter weights (1-9) <weights must total to 10> for: "
+					<< "\n\t Assignment Programs: ";
 				cin >> numsOf;
 				smt.setWeigths(numsOf, 0);
 				cout << "\n\t Tests: ";
 				cin >> numsOf;
 				smt.setWeigths(numsOf, 1);
-				cout << "\n\t FInals: ";
+				cout << "\n\t Finals: ";
 				cin >> numsOf;
 				smt.setWeigths(numsOf, 2);
 				total = 0;
 				for (int i = 0; i < 3; i++)
 					total += smt.getWeights(i);
 				if (total != 10) {
-					cout << "Weights not accepted.\n" << endl;
+					cout << "Weights do not equal 10" << endl
+						 << "Weights not accepted.\n" << endl;
 				}
 			} while (total != 10);
 			sFlag = true;
 			break;
 		case 'A':
 			if (sFlag) {
-				cout << "\nPlease enter Student's Last Name: ";
-				cin >> name;
-				stu.setLast(name);
+				Student *std = new Student();
+				string firstName;
+				string lastName;
+				int studentID;
+				do {
+					cout << "Enter student first name (20 characters max): ";
+					cin >> firstName;
+					cout << "\n" << endl;
+					if (firstName.size() < 20 && firstName.size() > 0) {
+						do {
+							cout << "Enter student last name (20 characters max): ";
+							cin >> lastName;
+							cout << "\n" << endl;
+							if (lastName.size() < 20 && lastName.size() > 0) {
+								do {
+									cout << "Enter student ID (1 - 9999): " << endl;
+									cin >> studentID;
+									cout << "\n" << endl;
+									if (studentID > 9999 || studentID < 0)
+										cout << "Invalid student ID\n" << endl;
+									else {
+										std->setFirst(firstName);
+										std->setLast(lastName);
+										std->setID(studentID);
+										if (std->getStdSize == 1) {
+											headStudent = std;
+											std->next = NULL;
+										}
+										else {
+											Student* temp;
+											Student* prev;
+											temp = headStudent;
+											string tempName = temp->getLast;
+											bool alpha = false; 
+											while (temp->next != NULL) {
+												for (int i = 0; i < 20; i++) {
+													char compA = std->getLast.get(i);
+													char compB = temp->getLast.getFirst(i);
 
-				cout << "\nPlease enter Student's First Name: ";
-				cin >> name;
-				stu.setFirst(name);
-
-				cout << "\nPlease enter student ID number: ";
-				cin >> numsOf;
-				stu.setID(numsOf);
+												}
+												prev = temp;
+												temp = temp->next;
+											}
+											prev->next = std;
+											std->next = NULL;
+										}
+									}
+								} while (studentID <= 9999 || studentID >= 0);
+							}
+							else
+								cout << "Invalid student last name\n" << endl;
+						} while (lastName.size() < 20 && lastName.size() > 0);
+					}
+					else
+						cout << "Invalid student first name\n" << endl;
+				} while (firstName.size() < 20 && firstName.size() > 0);
 			}
 			else {
 				cout << "Please enter a semester first." << endl;
@@ -111,7 +130,16 @@ void runBookMenu() {
 			break;
 		case 'P':
 			if (sFlag) {
-				//
+				do {
+					cout << "Enter the program number you wish to access: ";
+					cin >> numsOf;
+					cout << "\n" << endl;
+					if (numsOf >= 0 && numsOf < smt.getSizeP) {
+						//
+					}
+					else
+						cout << "The program number you have entered is invalid.\n" << endl;
+				} while (numsOf >= 0 && numsOf < smt.getSizeP);
 			}
 			else {
 				cout << "Please enter a semester first." << endl;
@@ -164,5 +192,6 @@ void runBookMenu() {
 			break;
 		}
 
-	} while (option != 'Q');
+	} while (option != 'Q' ||
+		option != 'q');
 }

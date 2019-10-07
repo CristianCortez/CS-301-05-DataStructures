36edb9a266018c7c8870392922e1075c5b750f2c
Kevin <55256219+Kevin-Nguyen-code@users.noreply.github.com>
2019-10-07 12:48:18 -0700

    Update interfaceMenu.cpp

--- Group Project/src/interfaceMenu.cpp
+++ Group Project/src/interfaceMenu.cpp
@@ -1,17 +1,11 @@
 #include "includes.h"
 
-void runDescription() {
-	cout << " This is an allergen database used to help users\n"
-		<< " build profiles for the various allergies of the human species.\n\n"
-		<< " Please make sure to have all information on hand to allow\n"
-		<< " easy data entry and access.\n" << endl;
-}
-
 //Search Function
 void searchMenu() {
 	int opt = 0;
 	string name, type, sciName;
-	int number;
+	int num;
+	DataBase db;
 
 	while (opt != 5) {
 		cout << "\n*************************"
@@ -23,34 +17,43 @@ void searchMenu() {
 			<< "\n* 5. Exit				*"
 			<< "\n*************************" << endl;
 		cin >> opt;
-	}
-	
-	switch (opt) {
+
+
+		switch (opt) {
 		case 1: // common name
 			cout << "Enter the name of the allergen you wish to search up: " << endl;
 			cin >> name;
-			break;
 
+			db.findComName(name);
+
+			break;
 		case 2: // allergen type
 			cout << "Enter the allergen type you wish to search up: " << endl;
 			cin >> type;
-			break;
 
+			db.findType(type);
+
+			break;
 		case 3: // nomenclature
 			cout << "Enter the Nomenclature you wish to search up: " << endl;
 			cin >> sciName;
-			break;
 
+			db.findSciName(sciName);
+
+			break;
 		case 4: // ncbi number
 			cout << "Enter the NCBI Number you wish to search up: " << endl;
-			cin >> number;
-			break;
+			cin >> num;
 
+			db.findNum(num);
+
+			break;
 		case 5: // exit
 			break;
 
 		default:
 			cout << "\tNot a valid option." << endl;
+		}
 	}
 	
 }
@@ -59,46 +62,53 @@ void searchMenu() {
 void deleteMenu() {
 	int opt = 0;
 	string name, sciName;
-	int number;
+	int num;
+	DataBase db;
 
 	while (opt != 4) {
 		cout << "\n***************************"
-			<< "\n*		Aller Delete Menu	*"
+			<< "\n*	AllerCo Delete Menu		*"
 			<< "\n*		1. Common Name		*"
 			<< "\n*		2. Nomenclature		*"
 			<< "\n*		3. NCBI Number		*"
 			<< "\n*		4. Exit				*"
 			<< "\n****************************" << endl;
-	}
 
-	switch (opt) {
+
+		switch (opt) {
 		case 1: // common name
 			cout << "Please enter the name of the allergen you wish to delete: " << endl;
 			cin >> name;
-			break;
 
+			break;
 		case 2: // Nomenclature
 			cout << "Please enter the Nomenclature of the allergen you wish to delete: " << endl;
 			cin >> sciName;
-			break;
 
+			break;
 		case 3: //NCBI Number
 			cout << "Please enter the NCBI Number of the allergen you wish to delete: " << endl;
-			cin >> number;
-			break;
+			cin >> num;
 
+			break;
 		case 4: //exit
 			break;
 
 		default:
 			cout << "\tNot a valid option" << endl;
 			break;
+		}
 	}
 }
 
+//Main Menu
 void runMainMenu() {
 	int option = 0;
 	bool sFlag = false;
+	string name, sciName, type;
+	int num;
+
+	DataBase db;
 
 	do{
 		cout << "\n*************************"
@@ -113,7 +123,19 @@ void runMainMenu() {
 		switch (option) {
 		case 1: // add
 			if (sFlag) {
+				cout << "Enter the common name of the allergen: " << endl;
+				cin >> name;
 
+				cout << "Enter the type of the allergen: " << endl;
+				cin >> type;
+
+				cout << "Enter the nomenclature of the allergen: " << endl;
+				cin >> sciName;
+
+				cout << "Enter the NCBI number of the allergen: " << endl;
+				cin >> num;
+
+				db.addAl(name, type, sciName, num);
 			}
 			else {
 				cout << "Please enter an allergen first." << endl;
@@ -121,7 +143,7 @@ void runMainMenu() {
 			break;
 		case 2: // delete
 			if (sFlag) {
-
+				deleteMenu();
 			}
 			else {
 				cout << "Please enter an allergen first." << endl;

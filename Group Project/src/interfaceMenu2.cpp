4a65aa3f1e9e8900444ce5806c144f47d9eb1e90
Kevin <55256219+Kevin-Nguyen-code@users.noreply.github.com>
2019-10-08 14:17:39 -0700

    Update interfaceMenu2.cpp

--- Group Project/src/interfaceMenu2.cpp
+++ Group Project/src/interfaceMenu2.cpp
@@ -23,6 +23,8 @@ char Menu::getMenu() {
 		<< endl;
 	cin >> choice;
 
+	putchar(toupper(choice));
+
 	return choice;
 }
 
@@ -36,6 +38,8 @@ char Menu::getDelete() {
 		<< "\n\tQ - Exits the menu "
 		<< endl;
 	cin >> choice;
+
+	putchar(toupper(choice));
 
 	return choice;
 }
@@ -52,11 +56,36 @@ char Menu::getSearch() {
 		<< endl;
 	cin >> choice;
 
+	putchar(toupper(choice));
+
 	return choice;
 }
 
 void Menu::setDelete(char input) {
+	string comName, sciName;
+	int num;
+	DataBase db;
 
+	//deletes by common name
+	if (input == 'C') {
+		cout << "Enter the common name of the allergen you wish to delete: " << endl;
+		cin >> comName;
+	}
+
+	// deletes by scientific name
+	else if (input == 'C') {
+		cout << "Enter the scientific name of the allergen you wish to delete: " << endl;
+		cin >> sciName;
+	}
+
+	//deletes by ncbi number
+	else if (input == 'N') {
+		cout << "Enter the NCBI number of the allergen you wish to delete: " << endl;
+		cin >> num;
+	}
+
+	else
+		exit(1);
 }
 
 void Menu::setSearch(char input) {
@@ -70,24 +99,28 @@ void Menu::setSearch(char input) {
 		cin >> comName;
 		db.findComName(comName);
 	}
+
 	//searches by scientific name
 	else if (input == 'S') {
 		cout << "Enter the Scientific Name of the allergen: " << endl;
 		cin >> sciName;
 		db.findSciName(sciName);
 	}
+
 	//searches by type
 	else if (input == 'T') {
 		cout << "Enter the type of the allergen: " << endl;
 		cin >> type;
 		db.findType(type);
 	}
+
 	//searches by ncbi number
 	else if (input == 'N') {
 		cout << "Enter the NCBI Number of the allergen: " << endl;
 		cin >> num;
 		db.findNum(num);
 	}
+
 	else
 		exit(1);
 }

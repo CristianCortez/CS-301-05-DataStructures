502691ba448fad0a62afb5f07103989643b96ce4
CristianCortez <49254113+CristianCortez@users.noreply.github.com>
2019-10-15 11:43:51 -0700

    Add files via upload

---
+++ Group Project/src/mainDriver.cpp
@@ -0,0 +1,58 @@
+#include "includes.h"
+
+int main() {
+	runDescription();
+	Menu opt;
+	char option, choice;
+
+	do {
+		char ch = opt.getMenu();
+
+		switch (ch) {
+		case 'A':
+			do {
+				opt.setAdd();
+
+				cout << "Do you want to add another allergen? (Y\N) " << endl;
+				cin >> choice;
+				choice = putchar(toupper(choice));
+			} while (choice != 'N');
+			break;
+		case 'D':
+			do {
+				option = opt.getDelete();
+				opt.setDelete(option);
+
+				cout << "Do you want to delete another allergen? (Y\N) " << endl;
+				cin >> choice;
+				choice = putchar(toupper(choice));
+			} while (choice != 'N');
+			break;
+		case 'S':
+			do {
+				option = opt.getSearch();
+				opt.setSearch(option);
+
+				cout << "Do you want to search another allergen? (Y\N) " << endl;
+				cin >> choice;
+				choice = putchar(toupper(choice));
+			} while (choice != 'N');
+			break;
+			/*case 'H':
+				opt.setSymptoms();
+				break;
+				*/
+		case 'Q':
+			//exit(1);
+			break;
+		default:
+			cout << ":: INVALID CHOICE ::" << endl;
+		}
+
+		cout << "Do you want to look at the menu again? (Y\N) " << endl;
+		cin >> choice;
+		choice = putchar(toupper(choice));
+	} while (choice != 'Q');
+
+	return 0;
+}
